95044da4830cbe6e7daaf765e6fe9c624743029f
Li Mingyu <e1484123@u.nus.edu>
2025-08-22 16:26:40 +0800

    Add Level-0: Rename the bot Talkist and implement greetings

---
+++ src/main/java/Talkist.java
@@ -0,0 +1,6 @@
+public class Talkist {
+    public static void main(String[] args) {
+        System.out.println("This is Talkist, a chat bot based on DUKE. How can I help you?\n");
+        System.out.println("Bye! See you soon.\n");
+    }
+}
