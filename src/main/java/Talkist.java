d86f4c00e7adc72fc002be6a3625e6230b183dbe
Li Mingyu <e1484123@u.nus.edu>
2025-08-23 21:25:04 +0800

    Add Level-1: Echo

--- src/main/java/Talkist.java
+++ src/main/java/Talkist.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Talkist {
     public static void main(String[] args) {
-        System.out.println("This is Talkist, a chat bot based on DUKE. How can I help you?\n");
-        System.out.println("Bye! See you soon.\n");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("This is Talkist, a chat bot based on DUKE. How can I help you?");
+
+        while (true) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Bye! See you soon.");
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+
+        sc.close();
     }
 }
